c8b33f04143b4f8b4f755107282a763af45317b5
Khalid Diriye <kdiriye18@gmail.com>
2018-07-06 17:04:57 -0500

    Backport HUE-4252

--- desktop/core/src/desktop/lib/rest/resource.py
+++ desktop/core/src/desktop/lib/rest/resource.py
@@ -119,20 +119,21 @@ class Resource(object):
 
     @return: A dictionary of the JSON result.
     """
-    return self.invoke("POST", relpath, params, data, self._make_headers(contenttype, headers))
+    return self.invoke("POST", relpath, params, data, self._make_headers(contenttype, headers), files, allow_redirects)
 
 
-  def put(self, relpath=None, params=None, data=None, contenttype=None):
+  def put(self, relpath=None, params=None, data=None, contenttype=None, allow_redirects=False):
     """
     Invoke the PUT method on a resource.
     @param relpath: Optional. A relative path to this resource's path.
     @param params: Key-value data.
     @param data: Optional. Body of the request.
     @param contenttype: Optional.
+    @param allow_redirects: Optional. Allow request to automatically resolve redirects.
 
     @return: A dictionary of the JSON result.
     """
-    return self.invoke("PUT", relpath, params, data, self._make_headers(contenttype))
+    return self.invoke("PUT", relpath, params, data, self._make_headers(contenttype), allow_redirects)
 
 
   def _make_headers(self, contenttype=None, headers=None):
